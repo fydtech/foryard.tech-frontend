2ac2514b68eadf8cb3147ae2f67fa8536fa86cb0
nathan <nathan@foryard.nl>
2018-09-19 14:47:59 +0200

    WIP animation of text #1

--- src/components/sections/BannerSection.vue
+++ src/components/sections/BannerSection.vue
@@ -1,11 +1,57 @@
 <template>
-  <div class="bg-red">
-    This is the Banner Section
+  <div class="bg-red offset-10">
+    <div>
+      <vue-anime-time-line
+        ref="demo11"
+        :seek="seek">
+        <vue-anime v-for="(key, index) in words"
+        :key="'key' + index"
+        class="square inline-block"
+        offset="-=200"
+        autoplay="true"
+        duration="500"
+        :animate="{
+          translateY: [ {value: -10, duration: 500 } ],
+          opacity: [{ value: 1 }]
+        }">{{ key }}</vue-anime>
+      </vue-anime-time-line>
+    </div>
   </div>
 </template>
 
 <script>
+import { VueAnime } from 'vue-anime'
+import { VueAnimeGroup } from 'vue-anime'
+import { VueAnimeTimeLine } from 'vue-anime'
+
 export default {
-  name: 'BannerSection'
+  name: 'BannerSection',
+  components: {
+    VueAnime,
+    VueAnimeGroup,
+    VueAnimeTimeLine
+  },
+  data() {
+    return {
+      sentence: 'Lorem ipsum dolor sit amet',
+      seek: null
+    };
+  },
+  computed: {
+    words() {
+      return this.sentence.split(" ");
+    }
+  }
 }
 </script>
+<style>
+  .word {
+    width: 10px;
+  }
+  .square, .circle {
+    opacity: 0;
+  }
+  .offset-10 {
+    transform: translateY(10px);
+  }
+</style>
