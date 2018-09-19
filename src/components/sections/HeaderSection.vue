945f52ea48bb40625aeb89cdc69b2d27a0cb9915
Arie Visser <arie.visser@gmail.com>
2018-09-19 15:11:20 +0200

    Header an products wireframe

---
+++ src/components/sections/HeaderSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-row bg-white">
+    <div class="flex-1">
+      Logo
+    </div>
+    <div class="flex-1">
+      Cases
+    </div>
+    <div class="flex-1">
+      Team
+    </div>
+    <div class="flex-1">
+      Contact
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSection'
+}
+</script>
---
+++ src/components/sections/ProductSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-row bg-grey-lightest">
+    <div class="flex-1 bg-grey-light m-10">
+      Webapplicaties
+    </div>
+    <div class="flex-1 bg-grey-light m-10">
+      Apps
+    </div>
+    <div class="flex-1 bg-grey-light m-10">
+      UX Design
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductSection'
+}
+</script>
