12823742e4d1d93e35d99a6f7edc275eb597a169
Arie Visser <arie.visser@gmail.com>
2018-09-21 11:02:37 +0200

    Products content

--- src/components/sections/ProductSection.vue
+++ src/components/sections/ProductSection.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="flex flex-row bg-grey-lightest">
-    <div class="flex-1 bg-grey-light m-10">
-      Webapplicaties
+  <div class="flex flex-col lg:flex-row pt-12">
+    <div class="flex-1 m-4">
+      <icon-with-text
+        title="Webapplicaties"
+        text="Razendsnelle webapplicaties met een CMS (en/of CRM) op maat. Eindeloos veel integraties met bestaande systemen en datasets."/>
     </div>
-    <div class="flex-1 bg-grey-light m-10">
-      Apps
+    <div class="flex-1 m-4">
+      <icon-with-text
+        title="Apps"
+        text="Progressive web apps voor iOS, Android en het web met ondersteuning voor native features."/>
     </div>
-    <div class="flex-1 bg-grey-light m-10">
-      UX Design
+    <div class="flex-1 m-4">
+      <icon-with-text
+        title="UX Design"
+        text="Design waarbij de gebruiker centraal staat met een sterke focus op eenvoud en gebruiksgemak."/>
     </div>
   </div>
 </template>
 
 <script>
+import IconWithText from '@/components/shared/IconWithText'
 export default {
-  name: 'ProductSection'
+  name: 'ProductSection',
+  components: {
+    IconWithText
+  }
 }
 </script>
