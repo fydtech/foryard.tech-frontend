b7c55f01419d510293303655688b7ba62dde210c
Gerwin Overeem <gerwin@foryard.nl>
2018-09-24 15:01:12 +0200

    Header scrollto options + hover effect

--- src/components/sections/HeaderSection.vue
+++ src/components/sections/HeaderSection.vue
@@ -2,30 +2,39 @@
   <div class="flex flex-col lg:flex-row bg-white px-20">
     <div class="flex">
       <div class="flex">
-        <img class="h-14 my-2" src="@/assets/img/foryard-logo-tech.svg">
+        <img class="h-14 my-4" src="@/assets/img/foryard-logo-tech.svg">
       </div>
     </div>
     <div class="flex flex-grow items-center lg:justify-end">
       <div class="flex mr-4 uppercase">
         <a
-          v-scroll-to="'#cases'"
-          class="no-underline text-black tracking-wide"
+          v-scroll-to="{
+            el: '#cases',
+            duration: 500
+          }"
+          class="no-underline text-black tracking-wide border-b-2 border-transparent hover:border-caribbean-green mt-1"
           href="#">
           Cases
         </a>
       </div>
       <div class="flex mx-4 uppercase">
         <a
-          v-scroll-to="'#team'"
-          class="no-underline text-black tracking-wide"
+          v-scroll-to="{
+            el: '#team',
+            duration: 1000
+          }"
+          class="no-underline text-black tracking-wide border-b-2 border-transparent hover:border-caribbean-green mt-1"
           href="#">
           Team
         </a>
       </div>
       <div class="flex mx-4 uppercase">
         <a
-          v-scroll-to="'#contact'"
-          class="no-underline text-black tracking-wide"
+          v-scroll-to="{
+            el: '#contact',
+            duration: 1500
+          }"
+          class="no-underline text-black tracking-wide border-b-2 border-transparent hover:border-caribbean-green mt-1"
           href="#">
           Contact
         </a>
