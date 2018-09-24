3aa4d9917a11db77dec239397d53ea2de88bdb0c
nathan <nathan@foryard.nl>
2018-09-24 15:53:24 +0200

    added icons and links in footer #16

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -3,12 +3,21 @@
     id="contact"
     class="bg-tangaroa">
     <div class="container mx-auto flex flex-col justify-between h-banner py-16 px-4">
-      <h1 class="uppercase text-4xl sm:text-5xl text-caribbean-green leading-none">Zin in koffie?</h1>
-      <p class="text-white text-lg w-full lg:w-1/2 leading-loose">Why on earth would we grab a coffee? Why on earth would we grab a coffee? Why on earth would we grab a coffee? Why on earth would we grab a coffee?</p>
-      <div class="ml-24 text-white text-lg leading-loose">
-        <div class="block">Phone</div>
-        <div class="block">email</div>
-        <div class="block">address</div>
+      <h1 class="uppercase md:text-4xl lg:text-5xl text-caribbean-green leading-none">Zin in koffie?</h1>
+      <p class="text-white text-lg w-full lg:w-1/2 leading-loose">Wij ook! Een goede kop koffie (of thee) is bij uitstek een prettige situatie om jouw vraag te bespreken. Laten we samen kijken naar de mogelijkheden en het realiseren van het juiste stukje software voor jouw organisatie.</p>
+      <div class="ml-24 text-white text-lg">
+        <div class="flex items-center">
+          <img src="@/assets/img/phone.svg" alt="phone_icon">
+          <a class="py-2 ml-6 text-white no-underline" href="tel:+3188 004 0600" rel="noreferrer nofollow">+31 (0) 88 004 0600</a>
+        </div>
+        <div class="flex items-center">
+          <img src="@/assets/img/email.svg" alt="email_icon">
+          <a class="py-2 ml-6 text-white no-underline" href="mailto:dev@foryard.tech" rel="noreferrer nofollow">dev@foryard.tech</a>
+        </div>
+        <div class="flex items-center">
+          <img src="@/assets/img/location.svg" alt="location_icon">
+          <a class="py-2 ml-6 text-white no-underline" href="https://goo.gl/maps/HpXX6YLcJtM2" rel="noreferrer nofollow">Kuipersstraat 35, 1074 EE Amsterdam</a>
+        </div>
       </div>
     </div>
     <div id="footer" class="bg-tangaroa-darker">
