8a0c610acdf2be3e059c65c5cc07e23821418f31
Arie Visser <arie.visser@gmail.com>
2018-09-24 15:07:07 +0200

    Team member layout

--- src/components/sections/TeamSection.vue
+++ src/components/sections/TeamSection.vue
@@ -1,20 +1,20 @@
 <template>
   <div
     id="team"
-    class="flex flex-col my-20">
-    <div class="flex-1 text-center">
+    class="flex flex-col my-20 text-center">
+    <div class="flex-1">
       <h1 class="uppercase">Team</h1>
-      <p class="py-12">Teambeschrijving</p>
     </div>
-    <div class="flex justify-center flex-wrap">
+    <div class="flex flex-wrap">
       <div
         v-for="member in members"
-        class="sm:width-full md:w-1/2 lg:w-1/3 text-center p-10">
+        class="flex lg:w-1/2 xl:w-1/3 flex-grow justify-center">
         <team-member
           :image="member.image"
           :name="member.name"
           :role="member.role"
-          :expertise="member.expertise" />
+          :expertise="member.expertise"
+          class="p-10" />
       </div>
     </div>
   </div>
