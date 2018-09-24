e9ed8adb224cc4a03d96bfef9047bf0398a0ca46
nathan <nathan@foryard.nl>
2018-09-24 13:29:18 +0200

    added footer design and height #16

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -1,34 +1,30 @@
 <template>
   <div
     id="contact"
-    class="flex flex-col bg-green pt-10 pl-10">
-    <div class="flex-1">
-      <h1>Let's grab a coffee</h1>
-    </div>
-    <div class="flex-1 ml-8 py-4">
-      <p>Why on earth would we grab a coffee?</p>
-    </div>
-    <div class="flex-1 ml-8">
-      Phone
-    </div>
-    <div class="flex-1 ml-8">
-      email
-    </div>
-    <div class="flex-1 ml-8">
-      address
-    </div>
-    <div class="flex flex-row mt-10">
-      <div class="flex mr-4">
-        Foryard
+    class="bg-tangaroa">
+    <div class="container mx-auto flex flex-col justify-between h-banner py-16 px-4">
+      <h1 class="uppercase text-4xl sm:text-5xl text-caribbean-green leading-none">Zin in koffie?</h1>
+      <p class="text-white text-lg w-full lg:w-1/2 leading-loose">Why on earth would we grab a coffee? Why on earth would we grab a coffee? Why on earth would we grab a coffee? Why on earth would we grab a coffee?</p>
+      <div class="ml-24 text-white text-lg leading-loose">
+        <div class="block">Phone</div>
+        <div class="block">email</div>
+        <div class="block">address</div>
       </div>
-      <div class="flex mx-4">
-        KvK
-      </div>
-      <div class="flex mx-4">
-        BTW
-      </div>
-      <div class="flex mx-4">
-        Privacy
+    </div>
+    <div id="footer" class="bg-tangaroa-darker">
+      <div class="container mx-auto h-20 flex justify-center lg:justify-start items-center text-white">
+        <div class="mx-4 lg:mr-4">
+          Foryard
+        </div>
+        <div class="mx-4">
+          KvK
+        </div>
+        <div class="mx-4">
+          BTW
+        </div>
+        <div class="mx-4">
+          Privacy
+        </div>
       </div>
     </div>
   </div>
