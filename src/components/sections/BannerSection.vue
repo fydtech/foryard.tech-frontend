596cecebb494527186fdbd5f082d7fb9ad2e7099
nathan <nathan@foryard.nl>
2018-09-26 13:13:55 +0200

    tooltip added #24

--- src/components/sections/BannerSection.vue
+++ src/components/sections/BannerSection.vue
@@ -1,111 +1,153 @@
 <template>
-  <div>
+  <div
+    id="banner"
+    :class="'relative flex flex-col relative py-16 h-vh-60 min-h-600'"
+    style="background-size: cover; "
+      :style="{ background: headerBgReady }">
     <div
-      id="banner"
-      :class="'relative flex flex-col relative py-16 h-banner min-h-500'"
-      style="background-size: cover; "
-        :style="{ background: headerBgReady }">
+      class="flex-1 text-left p-5 md:p-8">
+      <vue-anime-time-line
+        ref="intro"
+        :playing="playing">
+        <h1>
+          <vue-anime
+            :duration="1100"
+            class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
+            autoplay="true"
+            :animate="{
+              translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' }],
+              opacity: [{ value: 1, duration: 1000 }],
+              scale: [{value: 1.5, duration: 100, easing: 'easeOutElastic' }, {value: 1, duration: 900, easing: 'easeOutElastic' }],
+            }">
+            {{ words(sentence1)[0] + '&nbsp;' }}
+          </vue-anime>
+          <vue-anime v-for="(key, index) in words(sentence1).slice(1)"
+            :duration="1000"
+            :key="'key' + index"
+            class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
+            autoplay="true"
+            offset="-=500"
+            :animate="{
+              translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' } ],
+              opacity: [{ value: 1, duration: 1000 }],
+              scale: [{value: 1.3, duration: 90, easing: 'easeOutElastic' }, {value: 1, duration: 810, easing: 'easeOutElastic' }],
+            }">{{ key + '&nbsp;' }}
+          </vue-anime>
+        </h1>
+        <h2 class="mt-6 leading-none">
+          <vue-anime v-for="(key, index) in words(sentence2)"
+            :duration="800"
+            offset="-=600"
+            :key="'key' + index"
+            class="opacity-0 inline-block font-sans font-hind font-normal text-white text-xl md:text-2xl lg:text-3xl"
+            autoplay="true"
+            :animate="{
+              translateY: [ {value: -25, duration: 800, easing: 'easeOutQuint' } ],
+              opacity: [{ value: 1, duration: 800 }]
+            }">{{ key + '&nbsp;' }}
+          </vue-anime>
+        </h2>
+      </vue-anime-time-line>
       <div
-        class="flex-1 text-left p-4 md:p-8">
-        <vue-anime-time-line
-          ref="intro"
-          :playing="playing">
-          <h1>
-            <vue-anime
-              :duration="1100"
-              class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
-              autoplay="true"
-              :animate="{
-                translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' }],
-                opacity: [{ value: 1, duration: 1000 }],
-                scale: [{value: 1.5, duration: 100, easing: 'easeOutElastic' }, {value: 1, duration: 900, easing: 'easeOutElastic' }],
-              }">
-              {{ words(sentence1)[0] + '&nbsp;' }}
-            </vue-anime>
-            <vue-anime v-for="(key, index) in words(sentence1).slice(1)"
-              :duration="1000"
-              :key="'key' + index"
-              class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
-              autoplay="true"
-              offset="-=500"
-              :animate="{
-                translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' } ],
-                opacity: [{ value: 1, duration: 1000 }],
-                scale: [{value: 1.3, duration: 90, easing: 'easeOutElastic' }, {value: 1, duration: 810, easing: 'easeOutElastic' }],
-              }">{{ key + '&nbsp;' }}
-            </vue-anime>
-          </h1>
-          <h2 class="mt-6 leading-none">
-            <vue-anime v-for="(key, index) in words(sentence2)"
-              :duration="800"
-              offset="-=600"
-              :key="'key' + index"
-              class="opacity-0 inline-block font-sans font-hind font-normal text-white text-xl md:text-2xl lg:text-3xl"
-              autoplay="true"
-              :animate="{
-                translateY: [ {value: -25, duration: 800, easing: 'easeOutQuint' } ],
-                opacity: [{ value: 1, duration: 800 }]
-              }">{{ key + '&nbsp;' }}
-            </vue-anime>
-          </h2>
-        </vue-anime-time-line>
-        <div class="flex flex-row m-10 justify-center md:justify-start">
+        class="flex flex-col justify-center md:justify-start">
+        <div class="flex items-center">
           <div
             @mouseenter="laravelLogoHover = true"
-            @mouseleave="laravelLogoHover = false"
-            class="flex mr-10">
-            <img
-              v-if="laravelLogoHover"
-              src="@/assets/img/laravel-hover.svg">
-            <img
-              v-else
-              src="@/assets/img/laravel.svg">
+            class="flex cursor-pointer transition-all-15 md:hover:scale-110">
+            <vue-anime
+              :delay="6000"
+              :autoplay="true"
+              :playing="!laravelLogoHover"
+              :loop="true"
+              :animate="{
+                translateX: [
+                {value: 0, duration: 100},
+                {value: 1, duration: 100},
+                {value: -2, duration: 100},
+                {value: 1, duration: 100},
+                {value: -2, duration: 100},
+                {value: 1, duration: 100},
+                {value: 0, duration: 100},
+                {value: 0, duration: 3000}]
+              }">
+              <img
+                class="w-16 h-16 mr-4 my-6"
+                src="@/assets/img/laravel-hover.svg">
+            </vue-anime>
           </div>
           <div
-            @mouseenter="vueLogoHover = true"
-            @mouseleave="vueLogoHover = false"
-            class="flex">
-            <img
-              v-if="vueLogoHover"
-              src="@/assets/img/vue-hover.svg">
-            <img
-              v-else
-              src="@/assets/img/vue.svg">
+            class="bg-tag-grey p-4 rounded items-center flex scale-x-0"
+            :class="{ 'w-48 transition-all-15 scale-x-100': laravelLogoHover }">
+            <span
+              class="font-plex italic text-white text-xs hover:text-caribbean-green">
+              Laravel: The PHP Framework For Web Artisans
+            </span>
           </div>
         </div>
-        <div class="flex text-center mt-10 md:mt-32">
-          <div class="flex-1">
-            <a
-              v-scroll-to="'#products'"
-              href="#">
-              <vue-anime
-                :animate="{
-                  translateY: [{
-                    value: 10,
-                    duration: 1000,
-                    easing: 'easeInQuad'
-                  },
-                  {
-                    value: 0,
-                    duration: 1000,
-                    easing: 'easeOutQuad'
-                  }]
-                }"
-                :loop="true">
-                <img
-                  class="p-4"
-                  src="@/assets/img/chevron-down.svg" />
+        <div class="flex items-center">
+          <div
+            @mouseenter="vueLogoHover = true"
+            class="flex cursor-pointer transition-all-15 md:hover:scale-110">
+            <vue-anime
+              :delay="6000"
+              :playing="!vueLogoHover"
+              :autoplay="true"
+              :loop="true"
+              :animate="{
+                translateX: [
+                {value: 0, duration: 3000},
+                {value: 1, duration: 100},
+                {value: -2, duration: 100},
+                {value: 1, duration: 100},
+                {value: -2, duration: 100},
+                {value: 1, duration: 100},
+                {value: -1, duration: 100},
+                {value: 0, duration: 100}]
+              }">
+              <img
+                class="w-16 h-16 mr-4 my-6"
+                src="@/assets/img/vue-hover.svg">
               </vue-anime>
-            </a>
+            </div>
+          <div
+            class="bg-tag-grey p-4 rounded items-center flex scale-x-0"
+            :class="{ 'w-48 transition-all-15 scale-x-100': vueLogoHover }">
+            <span
+              class="font-plex italic text-white text-xs hover:text-caribbean-green">
+              Vue JS: The Progressive JavaScript Framework
+          </span>
           </div>
         </div>
       </div>
+    </div>
+    <div class="absolute pin-b pin-l text-center w-full mb-6">
+      <a
+        v-scroll-to="'#products'"
+        href="#">
+        <vue-anime
+          :animate="{
+            translateY: [{
+              value: 10,
+              duration: 1000,
+              easing: 'easeInQuad'
+            },
+            {
+              value: 0,
+              duration: 1000,
+              easing: 'easeOutQuad'
+            }]
+          }"
+          :loop="true">
+          <img
+            class="p-4"
+            src="@/assets/img/chevron-down.svg" />
+        </vue-anime>
+      </a>
     </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
 import headerBg from '@/assets/img/header-bg.jpg'
 import { VueAnime } from 'vue-anime'
 import { VueAnimeGroup } from 'vue-anime'
@@ -136,21 +178,6 @@ export default {
     .then(() => {
       this.headerBgReady = `linear-gradient(rgba(26, 36, 47, 0.9), rgba(26, 36, 47, 0.9)), url(${headerBg})`;
       this.playing = true;
-      setTimeout(() => {
-        this.laravelLogoHover = true;
-      }, 2500);
-
-      setTimeout(() => {
-        this.laravelLogoHover = false;
-      }, 2700);
-
-      setTimeout(() => {
-        this.vueLogoHover = true;
-      }, 2900);
-
-      setTimeout(() => {
-        this.vueLogoHover = false;
-      }, 3100);
     })
   },
   methods: {
@@ -161,7 +188,20 @@ export default {
       return new Promise(resolve => {
         imageElement.onload = resolve;
       })
+    },
+    random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     }
   }
 }
 </script>
+<style>
+.fade-enter-active {
+  transition: opacity 0.3s
+}
+
+.fade-leave-active {
+  transition: opacity 0.5s
+}
+</style>
+
