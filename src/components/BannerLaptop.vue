20560d381de0120756eceff25c2e3df0a56426f9
nathan <nathan@foryard.nl>
2018-09-27 18:37:50 +0200

    added animation to laptop

---
+++ src/components/BannerLaptop.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="relative bg-blue z-0">
+    <div class="absolute z-0">
+      <img src="@/assets/img/laptop.png" alt="laptop">
+    </div>
+    <div class="absolute z-10">
+      <vue-anime
+        :autoplay="true"
+        :playing="true"
+        :loop="true"
+        style="clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);"
+        :animate="{
+          clipPath: [
+          { value: 'polygon(0 0, 100% 0, 100% 0, 0 0)', duration: 1000, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 20%, 0 15%)', duration: 800, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 20%, 0 15%)', duration: 3000, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 40%, 0 30%)', duration: 800, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 40%, 0 30%)', duration: 3000, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 55%, 0 35%)', duration: 800, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 55%, 0 35%)', duration: 3000, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 75%, 0 50%)', duration: 800, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 75%, 0 50%)', duration: 3000, easing: 'linear'},
+          { value: 'polygon(0 0, 100% 0, 100% 100%, 0 100%0)', duration: 3000, easing: 'linear'}
+          ]
+        }">
+        <img src="@/assets/img/code.png" alt="code">
+      </vue-anime>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueAnime } from 'vue-anime'
+import { VueAnimeGroup } from 'vue-anime'
+import { VueAnimeTimeLine } from 'vue-anime'
+
+export default {
+  name: "BannerLaptop",
+  components: {
+    VueAnime,
+    VueAnimeGroup,
+    VueAnimeTimeLine
+  },
+  methods: {
+    codePlaying() {
+      console.log("play");
+      return true;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
--- src/components/sections/BannerSection.vue
+++ src/components/sections/BannerSection.vue
@@ -1,136 +1,146 @@
 <template>
   <div
     id="banner"
-    :class="'relative flex flex-col justify-center relative py-16 h-vh-60 min-h-500'"
-    style="background-size: cover; "
+    :class="'relative flex flex-col justify-center relative py-16 h-banner md:h-vh-60'"
+    style="background-size: cover;"
       :style="{ background: headerBgReady }">
     <div
-      class="text-center md:text-left p-5 md:p-8 sm:px-10 lg:px-20">
-      <vue-anime-time-line
-        ref="intro"
-        :playing="playing">
-        <h1>
-          <vue-anime
-            :duration="1100"
-            class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
-            autoplay="true"
-            :animate="{
-              translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' }],
-              opacity: [{ value: 1, duration: 1000 }],
-              scale: [{value: 1.5, duration: 100, easing: 'easeOutElastic' }, {value: 1, duration: 900, easing: 'easeOutElastic' }],
-            }">
-            {{ words(sentence1)[0] + '&nbsp;' }}
-          </vue-anime>
-          <vue-anime v-for="(key, index) in words(sentence1).slice(1)"
-            :duration="1000"
-            :key="'key' + index"
-            class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
-            autoplay="true"
-            offset="-=500"
-            :animate="{
-              translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' } ],
-              opacity: [{ value: 1, duration: 1000 }],
-              scale: [{value: 1.3, duration: 90, easing: 'easeOutElastic' }, {value: 1, duration: 810, easing: 'easeOutElastic' }],
-            }">{{ key + '&nbsp;' }}
-          </vue-anime>
-        </h1>
-        <h2 class="my-6 leading-none">
-          <vue-anime v-for="(key, index) in words(sentence2)"
-            :duration="800"
-            offset="-=600"
-            :key="'key' + index"
-            class="opacity-0 inline-block font-sans font-hind font-normal text-white text-xl md:text-2xl lg:text-3xl"
-            autoplay="true"
-            :animate="{
-              translateY: [ {value: -25, duration: 800, easing: 'easeOutQuint' } ],
-              opacity: [{ value: 1, duration: 800 }]
-            }">{{ key + '&nbsp;' }}
-          </vue-anime>
-        </h2>
-        <div
-          class="flex flex-col justify-center">
-          <vue-anime
-            :duration="800"
-            offset="+=100"
-            autoplay="true"
-            :animate="{
-              translateY: [ {value: -25, duration: 800, easing: 'easeOutQuint' } ],
-              opacity: [{ value: 1, duration: 800 }]
-            }"
-            class="opacity-0 scale-0">
-            <div class="flex items-center justify-center md:justify-start">
-              <div
-                @mouseenter="laravelLogoHover = true; laravelVibrate = false"
-                @mouseleave="laravelLogoHover = false"
-                class="flex items-center cursor-pointer transition-all-15 md:hover:scale-110 md:hover:rotate-15"
-                :class="{'rotate-0': laravelVibrate === false}">
-                <vue-anime
-                  class="flex items-center rotate-0"
-                  :delay="4000"
-                  :autoplay="true"
-                  :playing="laravelVibrate"
-                  :loop="true"
-                  :animate="{
-                    rotate: [
-                      {value: 0, duration: 10000},
-                      {value: '15deg', duration: 900},
-                      {value: 0, duration: 900}],
-                  }">
-                  <img
-                    class="w-16 h-16 mr-4 my-6"
-                    src="@/assets/img/laravel-hover.svg">
-                </vue-anime>
-              </div>
-              <div
-                class="bg-tag-grey p-4 rounded items-center flex scale-x-0"
-                :class="{ 'w-48 transition-all-15 scale-x-100': laravelLogoHover }">
-                <span
-                  class="font-plex italic text-white text-xs hover:text-caribbean-green">
-                  Laravel: The PHP Framework For Web Artisans
-                </span>
-              </div>
-            </div>
-            <div class="flex items-center justify-center md:justify-start">
-              <div
-                @mouseenter="vueLogoHover = true; vueVibrate = false"
-                @mouseleave="vueLogoHover = false"
-                class="flex items-center cursor-pointer transition-all-15 md:hover:scale-110 md:hover:rotate--15"
-                  :class="{'rotate-0': vueVibrate === false}">
-                <vue-anime
-                  class="flex items-center"
-                  :delay="5000"
-                  :playing="vueVibrate"
-                  :autoplay="true"
-                  :loop="true"
-                  :animate="{
-                    translateX: [
-                    {value: 2, duration: 100},
-                    {value: -2, duration: 100},
-                    {value: 2, duration: 100},
-                    {value: -2, duration: 100},
-                    {value: 2, duration: 100},
-                    {value: -2, duration: 100},
-                    {value: 0, duration: 100}]
-                  }">
-                  <img
-                    class="w-16 h-16 mr-4 my-6"
-                    src="@/assets/img/vue-hover.svg">
+      id="split-screen"
+      class="flex flex-wrap p-5 md:p-8 sm:px-10 lg:px-20">
+      <div
+        id="intro-animation"
+        class="w-full md:w-1/2 text-center md:text-left">
+        <vue-anime-time-line
+          ref="intro"
+          :playing="playing">
+          <h1>
+            <vue-anime
+              :duration="1100"
+              class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
+              autoplay="true"
+              :animate="{
+                translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' }],
+                opacity: [{ value: 1, duration: 1000 }],
+                scale: [{value: 1.5, duration: 100, easing: 'easeOutElastic' }, {value: 1, duration: 900, easing: 'easeOutElastic' }],
+              }">
+              {{ words(sentence1)[0] + '&nbsp;' }}
+            </vue-anime>
+            <vue-anime v-for="(key, index) in words(sentence1).slice(1)"
+              :duration="1000"
+              :key="'key' + index"
+              class="opacity-0 inline-block font-montserrat font-extrabold uppercase text-caribbean-green md:text-4xl lg:text-5xl"
+              autoplay="true"
+              offset="-=500"
+              :animate="{
+                translateY: [ {value: -25, duration: 1000, easing: 'easeOutQuint' } ],
+                opacity: [{ value: 1, duration: 1000 }],
+                scale: [{value: 1.3, duration: 90, easing: 'easeOutElastic' }, {value: 1, duration: 810, easing: 'easeOutElastic' }],
+              }">{{ key + '&nbsp;' }}
+            </vue-anime>
+          </h1>
+          <h2 class="my-6 leading-none">
+            <vue-anime v-for="(key, index) in words(sentence2)"
+              :duration="800"
+              offset="-=600"
+              :key="'key' + index"
+              class="opacity-0 inline-block font-sans font-hind font-normal text-white text-xl md:text-2xl lg:text-3xl"
+              autoplay="true"
+              :animate="{
+                translateY: [ {value: -25, duration: 800, easing: 'easeOutQuint' } ],
+                opacity: [{ value: 1, duration: 800 }]
+              }">{{ key + '&nbsp;' }}
+            </vue-anime>
+          </h2>
+          <div
+            class="flex flex-col justify-center">
+            <vue-anime
+              :duration="800"
+              offset="+=100"
+              autoplay="true"
+              :animate="{
+                translateY: [ {value: -25, duration: 800, easing: 'easeOutQuint' } ],
+                opacity: [{ value: 1, duration: 800 }]
+              }"
+              class="opacity-0 scale-0">
+              <div class="flex items-center justify-center md:justify-start">
+                <div
+                  @mouseenter="laravelLogoHover = true; laravelVibrate = false"
+                  @mouseleave="laravelLogoHover = false"
+                  class="flex items-center cursor-pointer transition-all-15 md:hover:scale-110 md:hover:rotate-15"
+                  :class="{'rotate-0': laravelVibrate === false}">
+                  <vue-anime
+                    class="flex items-center rotate-0"
+                    :delay="4000"
+                    :autoplay="true"
+                    :playing="laravelVibrate"
+                    :loop="true"
+                    :animate="{
+                      rotate: [
+                        {value: 0, duration: 10000},
+                        {value: '15deg', duration: 900},
+                        {value: 0, duration: 900}],
+                    }">
+                    <img
+                      class="w-16 h-16 mr-4 my-6"
+                      src="@/assets/img/laravel-hover.svg">
                   </vue-anime>
                 </div>
-              <div
-                class="bg-tag-grey p-4 rounded items-center flex scale-x-0"
-                :class="{ 'w-48 transition-all-15 scale-x-100': vueLogoHover }">
-                <span
-                  class="font-plex italic text-white text-xs hover:text-caribbean-green">
-                  Vue JS: The Progressive JavaScript Framework
-              </span>
+                <div
+                  class="bg-tag-grey p-4 rounded items-center flex scale-x-0"
+                  :class="{ 'w-48 transition-all-15 scale-x-100': laravelLogoHover }">
+                  <span
+                    class="font-plex italic text-white text-xs hover:text-caribbean-green">
+                    Laravel: The PHP Framework For Web Artisans
+                  </span>
+                </div>
               </div>
-            </div>
-          </vue-anime>
-        </div>
-      </vue-anime-time-line>
+              <div class="flex items-center justify-center md:justify-start">
+                <div
+                  @mouseenter="vueLogoHover = true; vueVibrate = false"
+                  @mouseleave="vueLogoHover = false"
+                  class="flex items-center cursor-pointer transition-all-15 md:hover:scale-110 md:hover:rotate--15"
+                    :class="{'rotate-0': vueVibrate === false}">
+                  <vue-anime
+                    class="flex items-center"
+                    :delay="5000"
+                    :playing="vueVibrate"
+                    :autoplay="true"
+                    :loop="true"
+                    :animate="{
+                      translateX: [
+                      {value: 2, duration: 100},
+                      {value: -2, duration: 100},
+                      {value: 2, duration: 100},
+                      {value: -2, duration: 100},
+                      {value: 2, duration: 100},
+                      {value: -2, duration: 100},
+                      {value: 0, duration: 100}]
+                    }">
+                    <img
+                      class="w-16 h-16 mr-4 my-6"
+                      src="@/assets/img/vue-hover.svg">
+                    </vue-anime>
+                  </div>
+                <div
+                  class="bg-tag-grey p-4 rounded items-center flex scale-x-0"
+                  :class="{ 'w-48 transition-all-15 scale-x-100': vueLogoHover }">
+                  <span
+                    class="font-plex italic text-white text-xs hover:text-caribbean-green">
+                    Vue JS: The Progressive JavaScript Framework
+                </span>
+                </div>
+              </div>
+            </vue-anime>
+          </div>
+        </vue-anime-time-line>
+      </div>
+      <div class="w-full md:w-1/2 self-stretch">
+        <banner-laptop />
+      </div>
     </div>
-    <div class="absolute pin-b pin-l text-center w-full mb-6">
+    <div
+      id="next-slide"
+      class="absolute pin-b pin-l text-center w-full mb-6">
       <a
         v-scroll-to="'#products'"
         href="#">
@@ -159,6 +169,7 @@
 
 <script>
 import headerBg from '@/assets/img/header-bg.jpg'
+import BannerLaptop from '@/components/BannerLaptop'
 import { VueAnime } from 'vue-anime'
 import { VueAnimeGroup } from 'vue-anime'
 import { VueAnimeTimeLine } from 'vue-anime'
@@ -168,7 +179,8 @@ export default {
   components: {
     VueAnime,
     VueAnimeGroup,
-    VueAnimeTimeLine
+    VueAnimeTimeLine,
+    BannerLaptop
   },
   data() {
     return {
